0a93b186d4ef6d0139fcb10909cb6a1508cb98cf
Coder <minifiedcode@gmail.com>
2019-07-28 02:00:46 -0500

    Changed background image and styled more components

--- src/home/index.js
+++ src/home/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './style.css'
+import background from '../img/background-2.jpeg'
 
 export default function Home() {
   return (
-    <div className="content">
-      <h1>Index</h1>
+    <div>
+      <div id="container">
+        <img id="bg" alt="background" src={background} />
+        <div className="content">
+          <div className="slogan-container">
+            <h1 id="welcome">Welcome to Two Brothers Lawn Care</h1>
+            <p id="motto"><i>Where speed is no longer a question</i></p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/navbar/index.js
+++ src/navbar/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './style.css'
 import Button from '../common/Button'
+import logo from '../img/gardening-30-512.png'
 
 const Navbar = () => (
   <div className="nav">
     {/* replace <p> element with <img> element */}
-    <img id="logo" alt={"Two Brothers Lawn Care"} src={"https://cdn0.iconfinder.com/data/icons/gardening-30/64/gardening-30-512.png"} />
-    <p id="company">Two Brothers Lawn Care</p>
+    <img id="logo" alt={"Two Brothers Lawn Care"} src={logo} />
     {/* routes to components */}
     <Button path="/" text="Home" type="nav" />
     <Button path="/about" text="About" type="nav" />
