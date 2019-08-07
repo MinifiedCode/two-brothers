48a56e6657fbbe6e19295ba9dd0e702b40957583
Coder <minifiedcode@gmail.com>
2019-08-06 20:58:40 -0500

    Cleaned up home component

---
+++ src/home/features.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './styles/features.css'
+
+export default class features extends Component {
+  render() {
+    return (
+      <div className="features-container">
+        
+      </div>
+    )
+  }
+}
--- src/home/index.js
+++ src/home/index.js
@@ -1,28 +1,17 @@
 import React from "react";
 import "./styles/style.css";
-import background from "../img/background-2.jpeg";
+import Welcome from './welcome'
 import About from './about'
 import Separator from '../common/Separator'
 import LawnImages from './lawnImages'
+import Features from './features'
 
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="container">
-        <img id="bg" alt="background" src={background} />
-        <div className="content">
-          <div className="slogan-container">
-            <h1 id="welcome">Welcome to Two Brothers Lawn Care</h1>
-            <p id="motto">
-              <i>"We treat everyone like family"</i>
-            </p>
-          </div>
-        </div>
-      </div>
-
+      <Welcome />
       <About />
       <Separator />
-
       <LawnImages />
     </div>
   );
