abcf13e7d4ebf9374febaff6a291585f4f4e284d
Caleb <minifiedcode@noreply>
2020-03-22 19:15:04 -0500

    Updated company email

--- src/footer/index.js
+++ src/footer/index.js
@@ -8,7 +8,7 @@ export default class Footer extends Component {
       <div className="footer">
         <div className="footer-container">
           <h1>{this.props.company}</h1>
-          <p id="companyEmail">mike@mowwichita.com</p>
+          <p id="companyEmail">info@twobrotherswichita.com</p>
           <p id="companyPhoneNum">316.650.4981</p>
         </div>
       </div>
